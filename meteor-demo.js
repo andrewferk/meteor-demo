56655e964e1d1de98ff75a6c8d0171c64757362c
Andrew Ferk <andrewferk@gmail.com>
2013-04-30 22:45:28 +0000

    Step 2 complete

---
+++ meteor-demo.js
@@ -0,0 +1,19 @@
+if (Meteor.isClient) {
+  Session.set('currentStep', 'step1');
+
+  Template.currentStep.renderedStep = function() {
+    return Template[Session.get('currentStep')]();
+  };
+
+  Template.step1.events({
+    'click #next-step': function(event) {
+      Session.set('currentStep', 'step2');
+    }
+  });
+
+  Template.step2.events({
+    'click #next-step': function(event) {
+      Session.set('currentStep', 'step3');
+    }
+  });
+}
