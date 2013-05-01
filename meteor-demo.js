51e5872bac9903580e1b7c6b46fb41db6c0e1e2d
Andrew Ferk <andrewferk@gmail.com>
2013-05-01 01:11:26 +0000

    Step 3 refactor

--- meteor-demo.js
+++ meteor-demo.js
@@ -1,19 +1,44 @@
 if (Meteor.isClient) {
-  Session.set('currentStep', 'step1');
-
-  Template.currentStep.renderedStep = function() {
-    return Template[Session.get('currentStep')]();
+  Session.set('currentStep', 3);
+  
+  var currentStep = function() {
+    return Session.get('currentStep');
+  };
+  
+  var nextStep = function() {
+    return Session.get('currentStep') + 1;
+  };
+  
+  var prevStep = function() {
+    return Session.get('currentStep') - 1;
   };
 
-  Template.step1.events({
-    'click #next-step': function(event) {
-      Session.set('currentStep', 'step2');
-    }
-  });
+  Template.currentStep.renderedStep = function() {
+    return Template['step' + currentStep()]();
+  };
 
-  Template.step2.events({
+  Template.nav.isNotFirst = function() {
+    return currentStep() !== 0;
+  };
+  
+  Template.nav.isNotLast = function() {
+    return Template['step' + nextStep()] !== undefined;
+  };
+  
+  Template.nav.nextStep = function() {
+    return nextStep();
+  };
+  
+  Template.nav.prevStep = function() {
+    return prevStep();
+  }
+
+  Template.nav.events({
     'click #next-step': function(event) {
-      Session.set('currentStep', 'step3');
+      Session.set('currentStep', nextStep());
+    },
+    'click #prev-step': function(event) {
+      Session.set('currentStep', prevStep());
     }
   });
 }
